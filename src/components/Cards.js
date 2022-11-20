2388968b61d53244ab4aa578d669ae0ed87ecec8
mateszucs96 <szucsmate096@gmail.com>
2022-11-20 23:55:34 +0100

    add function to handle load more button

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,30 +1,27 @@
 import Card from './Card';
 import styles from './cards.module.scss';
 import { DataContext } from '../helpers/data-context';
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
 
 const Cards = () => {
   const { data, handleDetailsPage } = useContext(DataContext);
-  let navigate = useNavigate();
+  const [visibleItems, setVisibleItems] = useState(12);
 
   const handleLoadMore = () => {
-    navigate(`/details/${data[0].id}`);
+    setVisibleItems((prevState) => prevState + 3);
   };
 
   return (
     <main>
       <div className={styles.cards}>
-        {data.map((company, i) => {
-          while (i < 12) {
-            return (
-              <Card
-                key={company.id}
-                data={company}
-                handleDetailsPage={handleDetailsPage}
-              />
-            );
-          }
+        {data.slice(0, visibleItems).map((company) => {
+          return (
+            <Card
+              key={company.id}
+              data={company}
+              handleDetailsPage={handleDetailsPage}
+            />
+          );
         })}
       </div>
       <button onClick={handleLoadMore} className={styles.button}>
